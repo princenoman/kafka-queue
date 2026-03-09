5618188f7ea3ea009919401ad9635a4561b61b92
princenoman <81908369+princenoman@users.noreply.github.com>
2026-03-09 23:02:19 +0600

    Add files via upload

--- src/KafkaQueue.php
+++ src/KafkaQueue.php
@@ -26,18 +26,18 @@ class KafkaQueue extends Queue implements QueueContract
         // $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($job));
         $topic->produce(RD_KAFKA_PARTITION_UA, 0, "Hello From the Ambassador APP");
 
-        $this->producer->flush(1000);
+        // $this->producer->flush(1000);
 
-        // $this->producer->poll(0);
+        $this->producer->poll(0);
 
-        // for ($retries = 0; $retries < 10; $retries++) {
-        //     $result = $this->producer->flush(1000);
-        //     if ($result === RD_KAFKA_RESP_ERR_NO_ERROR) {
-        //         return true;
-        //     }
-        // }
+        for ($retries = 0; $retries < 10; $retries++) {
+            $result = $this->producer->flush(1000);
+            if ($result === RD_KAFKA_RESP_ERR_NO_ERROR) {
+                return true;
+            }
+        }
 
-        // throw new \RuntimeException('Kafka producer flush failed');
+        throw new \RuntimeException('Kafka producer flush failed');
 
     }
 
