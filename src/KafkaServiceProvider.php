d752ea8bc2f0e06f4fafb78ac261c74e186cc75a
princenoman <81908369+princenoman@users.noreply.github.com>
2026-03-09 15:04:11 +0600

    Add files via upload

---
+++ src/KafkaServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Kafka;
+
+use Illuminate\Support\ServiceProvider;
+
+class KafkaServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $manager = $this->app['queue'];
+
+        $manager->addConnector('kafka', function () {
+            return new KafkaConnector;
+        });
+    }
+}
